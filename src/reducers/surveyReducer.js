b26abf9c5e10ed788e990e5f032042d762128b29
Leslie Stevens <flymaven@protonmail.com>
2021-02-03 19:59:49 -0600

    edit surveyReducer for response

--- src/reducers/surveyReducer.js
+++ src/reducers/surveyReducer.js
@@ -1,12 +1,23 @@
 export default function surveyReducer(state = {surveys: []}, action) {
   switch (action.type) {
     case "FETCH_SURVEYS":
-
+      //debugger;
       return {surveys: action.payload.surveys.data}
       //debugger;
-      case "CREATE_SURVEY":
+    case "CREATE_SURVEY":
       //debugger;
-        return {...state, surveys: [...state.surveys, action.payload.survey.data]}
+      return {...state, surveys: [...state.surveys, action.payload.survey.data]}
+    case "ADD_RESPONSE":
+    console.log(action)
+    //debugger;
+    //  let surveys = state.accounts.map(survey => {
+    //    if (survey.id === action.payload.response.data.survey_id) {
+    //      return action.payload
+    //    } else {
+    //      return
+      //  }
+
+
     default:
       return state
   }
