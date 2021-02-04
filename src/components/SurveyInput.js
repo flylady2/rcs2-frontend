92ef9253c556997fcf7d438d008e0d73b730bc69
Leslie Stevens <flymaven@protonmail.com>
2021-02-03 19:58:40 -0600

    clean up SurveyInput

--- src/components/SurveyInput.js
+++ src/components/SurveyInput.js
@@ -2,30 +2,47 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {createSurvey} from '../actions/createSurvey'
 
+let initialScores = {
+  choiceAInitialScore: 0,
+  choiceBInitialScore: 0,
+  choiceCInitialScore: 0,
+  choiceDInitialScore: 0
+}
+
 class SurveyInput extends React.Component {
 
-  state = {name: "",
-    user_id: 1,
-    choiceAContent: "",
-    choiceBContent: "",
-    choiceAInitialScore: 0,
-    choiceBInitialScore: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      user_id: 1,//this.props.userId
+      name: "",
+      choiceAContent: "",
+      choiceBContent: "",
+      choiceCContent: "",
+      choiceDContent: ""
+    }
   }
+
 
   handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value,
-      [event.target.user_id]: event.target.user_id,
-      [event.target.choiceAContent]: event.target.choiceAContent,
-      [event.target.choiceAContent]: event.target.choiceAContent,
-      choiceAInitialScore: 0,
-      choiceBInitialScore: 0
+      //[event.target.user_id]: event.target.user_id,
+      //[event.target.choiceAContent]: event.target.choiceAContent,
+      //[event.target.choiceAContent]: event.target.choiceAContent,
+      //choiceAInitialScore: 0,
+      //choiceBInitialScore: 0
     })
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.createSurvey(this.state)
+    let survey = {
+      //user_id: parseInt(this.props.user.id),
+      ...this.state,
+      ...initialScores
+    }
+    this.props.createSurvey(survey)
   }
 
 
@@ -39,20 +56,18 @@ class SurveyInput extends React.Component {
           <label>Survey Name:</label>
           <input name="name" type="text" value={this.state.name} onChange={this.handleChange}/>
           <br/>
-          <label>user_id:</label>
-          <input name="user_id" type="number" value={this.state.user_id} onChange={this.handleChange}/>
-          <br/>
-          <label>ChoiceA:</label>
+          <label>Choice:</label>
           <input name="choiceAContent" type="content" value={this.state.choiceAContent} onChange={this.handleChange}/>
-          <br/>
-          <label>ChoiceA initial score:</label>
-          <input name="choiceAScore" type="number" />
           <br/>
           <label>Choice:</label>
           <input name="choiceBContent" type="content" value={this.state.choiceBContent} onChange={this.handleChange}/>
           <br/>
-          <label>ChoiceB initial score:</label>
-          <input name="choiceBscore" type="number" />
+          <label>Choice:</label>
+          <input name="choiceCContent" type="content" value={this.state.choiceCContent} onChange={this.handleChange}/>
+          <br/>
+          <label>Choice:</label>
+          <input name="choiceDContent" type="content" value={this.state.choiceDContent} onChange={this.handleChange}/>
+
           <input type="submit"></input>
         </form>
       </div>
