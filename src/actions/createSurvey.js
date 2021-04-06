0284f867c93bca35cbdb43ca10a5cfbe1c7f40cb
Leslie Stevens <flymaven@protonmail.com>
2021-04-06 17:39:27 -0500

    add form to create survey with variable number of choices'

--- src/actions/createSurvey.js
+++ src/actions/createSurvey.js
@@ -1,5 +1,5 @@
 export function createSurvey(data) {
-  //debugger;
+  
   return (dispatch) => {
     fetch('http://localhost:3000/api/v1/surveys',{
       headers: {
--- src/containers/SurveysContainer.js
+++ src/containers/SurveysContainer.js
@@ -4,7 +4,7 @@ import {Route, Switch} from 'react-router-dom';
 import {fetchSurveys} from '../actions/fetchSurveys'
 import Surveys from '../components/Surveys';
 import Survey from '../components/Survey';
-import SurveyInput from '../components/SurveyInput';
+import NewSurveyInput from '../components/NewSurveyInput';
 
 class SurveysContainer extends React.Component {
 
@@ -20,7 +20,7 @@ class SurveysContainer extends React.Component {
     return (
       <div>
         <Switch>
-          <Route path='/surveys/new' component={SurveyInput}/>
+          <Route path='/surveys/new' component={NewSurveyInput}/>
 
           <Route path='/surveys/:name' render={(routerProps) =>
             <Survey {...routerProps} surveys={this.props.surveys}/>}/>
