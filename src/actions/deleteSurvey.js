d4c731e82601c3f999beb4825901ae1f51e19ce5
Leslie Stevens <flymaven@protonmail.com>
2021-04-08 17:49:17 -0500

    add action to delete survey

---
+++ src/actions/deleteSurvey.js
@@ -0,0 +1,20 @@
+export function deleteSurvey(idObj) {
+
+  return (dispatch) => {
+
+    fetch(`http://localhost:3000/api/v1/surveys/${idObj.id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      method: "DELETE",
+      body: JSON.stringify(idObj)
+    })
+    .then(resp => resp.json())
+    .then(idObj => dispatch({
+      type: 'DELETE_SURVEY',
+      payload: idObj
+    }))
+
+  }
+}
