96510e6739926a676601aac71615ce34181d8176
Leslie Stevens <flymaven@protonmail.com>
2021-04-07 21:09:51 -0500

    add UserFunctionsContainer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import SurveysContainer from './containers/SurveysContainer'
+import SurveysContainer from './containers/SurveysContainer';
+import UserFunctionsContainer from './containers/UserFunctionsContainer'
 
 
 
@@ -20,6 +21,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <SurveysContainer/>
+        <UserFunctionsContainer/>
 
       </div>
     );
